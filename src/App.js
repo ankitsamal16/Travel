9d1a48e6521706fb7ba5edfdf01870cf84e0fe90
AnkitSamal16 <126022706+AnkitSamal16@users.noreply.github.com>
2024-02-07 20:20:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Homepage from './Components/Homepage/Homepage';
+import Main from './Components/Main/Main';
+import Footer from './Components/Footer/Footer';
+
+
+const App = () => {
+  return(
+    <>
+    <Navbar/>
+    <Homepage/>
+    <Main/>
+    <Footer/>
+    </>
+  )
+}
+export default App;
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,158 @@
+import React,{useEffect} from 'react'
+import './Footer.css';
+import video2 from '../../Assets/video(2).mp4'
+import {FiSend} from 'react-icons/fi'
+import {MdOutlineTravelExplore} from 'react-icons/md'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {AiFillYoutube} from 'react-icons/ai'
+import {AiFillInstagram} from 'react-icons/ai'
+import {FaTripadvisor} from 'react-icons/fa'
+import {FiChevronRight} from 'react-icons/fi'
+
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
+const Footer = () => {
+
+  // React hook to create scroll fade animation
+  useEffect(()=>{
+    Aos.init({duration:2000})
+  })
+
+  return (
+    <section className='footer'>
+      <div className="videoDiv">
+        <video src={video2} muted autoPlay loop type="video/mp4"></video>
+      </div>
+
+      <div className="secContent container">
+        <div className="contactDiv flex">
+          <div data-aos="fade-up" className="text">
+            <small>KEEP IN TOUCH</small>
+            <h2>Travel With Us</h2>
+          </div>
+
+          <div className="inputDiv flex">
+            <input data-aos="fade-up" type="text" placeholder='Enter Email Address'/>
+            <button data-aos="fade-up" className='btn flex' type='submit'>
+              SEND<FiSend className="icon"/>
+            </button>
+          </div>
+        </div>
+
+        <div className="footerCard flex">
+          <div className="footerIntro flex">
+            <div className="logoDiv">
+              <a href="/" className='logo flex'>
+                <MdOutlineTravelExplore className='icon'/> Travel
+              </a>
+            </div>
+
+            <div data-aos="fade-up" className="footerParagraph">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio distinctio accusantium necessitatibus sint, eum impedit aperiam mollitia ipsam corrupti tempora doloremque voluptatibus porro dignissimos rem velit doloribus illo quaerat quo!
+            </div>
+
+            <div data-aos="fade-up" className="footerSocials flex">
+              <AiOutlineTwitter className='icon'/>
+              <AiFillYoutube className='icon'/>
+              <AiFillInstagram className='icon'/>
+              <FaTripadvisor className='icon'/>
+            </div>
+          </div>
+
+{/* Group 1 */}
+          <div data-aos="fade-up" data-aos-duration="3000" className="footerLinks grid">
+            <div className="linkGroup">
+              <span className="groupTitle">
+                OUR AGENCY
+              </span>
+
+              <li className='footerList flex'>
+                <FiChevronRight className='icon'/>
+                Services
+              </li>
+              <li className='footerList flex'>
+                <FiChevronRight className='icon'/>
+                Insurance
+              </li>
+              <li className='footerList flex'>
+                <FiChevronRight className='icon'/>
+                Agency
+              </li>
+              <li className='footerList flex'>
+                <FiChevronRight className='icon'/>
+                Tourism
+              </li>
+              <li className='footerList flex'>
+                <FiChevronRight className='icon'/>
+                Payment
+              </li>
+            </div>
+{/* Group 2 */}
+            <div data-aos="fade-up" data-aos-duration="4000" className="linkGroup">
+              <span className="groupTitle">
+                PARTNERS
+              </span>
+ 
+              <li className='footerList flex'>
+                <FiChevronRight className='icon'/>
+                Bookings
+              </li>
+              <li className='footerList flex'>
+                <FiChevronRight className='icon'/>
+                HostelWorld
+              </li>
+              <li className='footerList flex'>
+                <FiChevronRight className='icon'/>
+                RentCars
+              </li>
+              <li className='footerList flex'>
+                <FiChevronRight className='icon'/>
+                Trivago
+              </li>
+              <li className='footerList flex'>
+                <FiChevronRight className='icon'/>
+                TripAdvisor
+              </li>
+            </div>
+{/* Group 3 */}
+            <div data-aos="fade-up" data-aos-duration="5000" className="linkGroup">
+              <span className="groupTitle">
+                LAST MINUTE
+              </span>
+
+              <li className='footerList flex'>
+                <FiChevronRight className='icon'/>
+                London
+              </li>
+              <li className='footerList flex'>
+                <FiChevronRight className='icon'/>
+                California
+              </li>
+              <li className='footerList flex'>
+                <FiChevronRight className='icon'/>
+                Indonesia
+              </li>
+              <li className='footerList flex'>
+                <FiChevronRight className='icon'/>
+                Europe
+              </li>
+              <li className='footerList flex'>
+                <FiChevronRight className='icon'/>
+                Oceania
+              </li>
+            </div>
+
+            <div className="footerDiv flex">
+              <small>BEST TRAVEL WEBSITE THEME</small>
+              <small>COPYRIGHTS RESERVED - ANKIT KUMAR SAMAL - 2023</small>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Footer
